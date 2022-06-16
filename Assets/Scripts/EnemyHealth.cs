64406e9a04df90795d3353db328d55c830fc8571
Drew <dcm752052@gmail.com>
2022-06-15 22:12:52 -0400

    Created health bar for enemy prefab.

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,14 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public float enemyHealth = 100f;
+    public float maxHealth = 100f;
+    public float enemyHealth;
+
+    public GameObject healthBarUI;
+    public Slider slider;
+
+    private void Start()
+    {
+        enemyHealth = maxHealth;
+        slider.value = CalculateHealth();
+    }
+
+    private void Update()
+    {
+        slider.value = CalculateHealth();
+
+        if (enemyHealth < maxHealth)
+        {
+            healthBarUI.SetActive(true);
+        }
+    }
 
     public void DamageEnemy(int amount)
     {
         enemyHealth -= amount;
         if (enemyHealth <= 0) { Destroy(gameObject); }
+    }
+
+    float CalculateHealth()
+    {
+        return enemyHealth / maxHealth;
     }
 }
