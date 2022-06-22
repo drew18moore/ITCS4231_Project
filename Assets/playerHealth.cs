56b502af052f2bcffe7f74de696d5b292ee39cf9
Drew <dcm752052@gmail.com>
2022-06-21 22:13:53 -0400

    Created a health bar for the player.

--- Assets/playerHealth.cs
+++ Assets/playerHealth.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerHealth : MonoBehaviour
 {
-    [SerializeField] private float playerHP = 100f;
+    public float maxPlayerHP = 100f;
+    private float playerHP;
+
+    public Slider slider;
+
+    private void Start()
+    {
+        playerHP = maxPlayerHP;
+        slider.value = CalculateHealth();
+    }
 
     private void Update()
     {
+        slider.value = CalculateHealth();
         Debug.Log(playerHP);
+
         if (playerHP <= 0f)
         {
             Debug.Log("PLAYER DEAD");
@@ -18,5 +30,10 @@ public class playerHealth : MonoBehaviour
     public void damagePlayer(float amount)
     {
         playerHP -= amount;
+    }
+
+    float CalculateHealth()
+    {
+        return playerHP / maxPlayerHP;
     }
 }
