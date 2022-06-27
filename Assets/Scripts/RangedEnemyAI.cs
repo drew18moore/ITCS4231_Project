730fa0ca25cad3812fefe9627ca68e55ffefc119
Drew <dcm752052@gmail.com>
2022-06-27 00:48:31 -0400

    Duplicated MeleeEnemyAI script and renamed it to RangedEnemyAI. Currently RangedEnemy acts like the mele enemy.

---
+++ Assets/Scripts/RangedEnemyAI.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RangedEnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject visor;
+    public Material redVisorMaterial;
+    public Material defaultMaterial;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    // Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    // Attacking
+    public float attackDamage;
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    // States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+    }
+
+    void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    void Patroling()
+    {
+        visor.GetComponent<Renderer>().material = defaultMaterial;
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+        visor.GetComponent<Renderer>().material = redVisorMaterial;
+    }
+
+    void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        visor.GetComponent<Renderer>().material = redVisorMaterial;
+
+        var playerPos = player.position;
+        playerPos.y = transform.position.y;
+        transform.LookAt(playerPos);
+
+        if (!alreadyAttacked)
+        {
+            GameObject.Find("Player").GetComponent<playerHealth>().damagePlayer(attackDamage);
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+}
