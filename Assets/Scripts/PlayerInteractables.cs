5b5e2668dc1a04ff6a1aece6483919e7ecbf00e0
Drew <dcm752052@gmail.com>
2022-06-27 01:53:34 -0400

    Created door to exit gate. When you look at the door, are in range, and presses interactkey, the door object is destroyed. Fixed bug where player cant jump on exit stairs.

---
+++ Assets/Scripts/PlayerInteractables.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractables : MonoBehaviour
+{
+    public KeyCode interactKey;
+    public float interactRange = 4f;
+    public Camera cam;
+
+    private bool hasKey = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, interactRange) && hit.transform.tag == "ExitDoor")
+        {
+            Debug.Log("LOOKING AT DOOR");
+            if (Input.GetKey(interactKey))
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
