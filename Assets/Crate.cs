9c135483c3322fb789dd68aca5fbdeb1d4c3a6e9
Drew <dcm752052@gmail.com>
2022-06-26 22:45:06 -0400

    Crates have a 50% chance to drop medkits. Fixed hole in wall. Placed crates around level.

--- Assets/Crate.cs
+++ Assets/Crate.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Crate : MonoBehaviour
 {
     public float hits = 2f;
+    public GameObject medkitPrefab;
 
     public void DamageCrate()
     {
         hits--;
-        if (hits <= 0) { Destroy(gameObject); }
+        if (hits <= 0) 
+        {
+            if (Random.value > 0.5) { Instantiate(medkitPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity); }
+            Destroy(gameObject);
+        }
     }
 }
