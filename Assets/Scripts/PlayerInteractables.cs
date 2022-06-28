97b00fe1fa69616e60f5a01737baa0de1ba6e1c3
Drew <dcm752052@gmail.com>
2022-06-27 22:04:51 -0400

    The chest will play open animation when pressing E on it.

--- Assets/Scripts/PlayerInteractables.cs
+++ Assets/Scripts/PlayerInteractables.cs
@@ -8,24 +8,48 @@ public class PlayerInteractables : MonoBehaviour
     public float interactRange = 4f;
     public Camera cam;
 
+    public GameObject chest;
+
     private bool hasKey = false;
 
     // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, interactRange) && hit.transform.tag == "ExitDoor")
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, interactRange))
         {
-            Debug.Log("LOOKING AT DOOR");
-            if (Input.GetKey(interactKey) && hasKey)
+            if (hit.transform.tag == "ExitDoor")
             {
-                Destroy(hit.transform.gameObject);
+                Debug.Log("LOOKING AT DOOR");
+                if (Input.GetKey(interactKey) && hasKey)
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+            else if (hit.transform.tag == "Chest")
+            {
+                Debug.Log("LOOKING AT CHEST");
+                if (Input.GetKey(interactKey))
+                {
+                    chest = hit.transform.gameObject;
+                    Debug.Log("OPEN CHEST");
+                    Animator anim = chest.GetComponent<Animator>();
+                    anim.Play("Open");
+                    StartCoroutine(stopAnimation(anim));
+                }
             }
         }
+
     }
 
     public void CollectKey()
     {
         hasKey = true;
+    }
+
+    IEnumerator stopAnimation(Animator anim)
+    {
+        yield return new WaitForSeconds(3f);
+        anim.speed = 0;
     }
 }
